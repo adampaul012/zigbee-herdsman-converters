1db9d8a9f3cf73fa6fe9fb68c042e09435add0d6
Adam Paul <74373613+adampaul012@users.noreply.github.com>
2025-10-17 11:29:19 +0200

    Add support for Niceboy ION

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -1585,7 +1585,10 @@ export const definitions: DefinitionWithExtend[] = [
         ],
     },
     {
-        fingerprint: tuya.fingerprint("TS0601", ["_TZE200_s1xgth2u"]),
+        fingerprint: tuya.fingerprint("TS0601", [
+            "_TZE200_s1xgth2u",
+            "_TZE200_t3xd7l44",
+        ]),
         model: "TS0601_temperature_humidity_sensor_3",
         vendor: "Tuya",
         description: "Temperature & humidity sensor",
